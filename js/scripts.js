037b6daf23d7c4d12075c761e7aeac890cb7be6e
Jiwon Kang and Kaz Matthews <jiwon@>
2017-01-10 11:08:44 -0800

    add jQuery and CSS to highlight and border html paragraphs.

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $("p#1").click(function() {
+    $("p").removeClass("highlight");
+    $("p#1").addClass("highlight");
+  });
+  $("p#2").click(function() {
+    $("p").removeClass("highlight");
+    $("p#2").addClass("highlight");
+  });
+  $("p#3").click(function() {
+    $("p").removeClass("highlight");
+    $("p#3").addClass("highlight");
+  });
+  $("p#4").click(function() {
+    $("p").removeClass("highlight");
+    $("p#4").addClass("highlight");
+  });
+  $("p#5").click(function() {
+    $("p").removeClass("highlight");
+    $("p#5").addClass("highlight");
+  });
+  $("p#6").click(function() {
+    $("p").removeClass("highlight");
+    $("p#6").addClass("highlight");
+  });
+  $("p#7").click(function() {
+    $("p").removeClass("highlight");
+    $("p#7").addClass("highlight");
+  });
+});
